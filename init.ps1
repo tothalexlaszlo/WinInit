2634949d35d095c0d9db05a1ac5fb1850e3acbe5
tothalexlaszlo <alex.toth21@gmail.com>
2023-01-01 19:36:39 +0100

    ask for restart

--- init.ps1
+++ init.ps1
@@ -5,3 +5,8 @@ wsl --install
 winget source update
 winget upgrade --all
 winget import -i apps.json --accept-package-agreements --accept-source-agreements
+
+ $answer = Read-Host -Prompt "Do you want to restart computer [y]es or [n]o (default)?"
+ if ($answer -ieq 'y') {
+    Restart-Computer
+ }
